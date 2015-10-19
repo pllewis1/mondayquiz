a36db3ea328a9f4f011f4a13d82b0041f1be1c5c
Patrick Lewis <pllewis1@gmail.com>
2015-10-19 11:34:05 -0500

    classwork2 is the one

---
+++ classwork.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+  var x = a + b;
+  return x;
+}
+
+var z= sum(2, 5);
+console.log(z);
+
+f
---
+++ classwork2.js
@@ -0,0 +1,9 @@
+function Dog (name, breed, nose, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.nose = nose;
+  this.weight = weight;
+}
+var walker = new Dog ("Walker", "Golden", "wet", "big");
+
+console.log (walker);
